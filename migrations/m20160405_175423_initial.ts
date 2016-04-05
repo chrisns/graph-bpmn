ef05a602bf95813c4d1c02a7bd1fa953dcd2b43e
Chris Nesbitt-Smith <chris@cns.me.uk>
2016-04-05 23:02:03 +0100

    initial bootstrap that makes a db up & travis docker setup to build a orientdb setup to test against

---
+++ migrations/m20160405_175423_initial.ts
@@ -0,0 +1,11 @@
+"use strict";
+exports.name = "initial";
+
+exports.up = function (db) {
+  // @todo implementation
+};
+
+exports.down = function (db) {
+  // @todo implementation
+};
+
--- test/bootstrap.test.ts
+++ test/bootstrap.test.ts
@@ -1,6 +1,40 @@
 "use strict";
 
 import * as chai from "chai";
+import * as OrientDB from "orientjs";
+import * as uuid from "uuid";
 
 chai.use(require("chai-as-promised"))
   .should();
+
+const server = OrientDB({
+  host: process.ENV['TRAVIS'] ? '127.0.0.1' : '192.168.99.100',
+  port: 2424,
+  username: 'root',
+  password: 'root'
+});
+
+const dbSettings = {
+  name: 'testDb',
+  type: 'graph',
+  storage: 'plocal'
+};
+
+before(() =>
+  server.create(dbSettings)
+    .catch(error => server.use(dbSettings.name))
+    .tap(db => {
+      global['db'] = db
+    })
+    .then(db => new OrientDB.Migration.Manager({
+        db: db,
+        dir: __dirname + '/../migrations'
+      })
+    )
+    .then(manager => manager.up(1))
+);
+
+after(() =>
+  server.drop(dbSettings)
+    .then(() => server.close())
+);
